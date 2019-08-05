3cb0f2d77f179a7a6c064d7716245e9775a114ad
taha hussainalash <taha.benzma94@gmail.com>
2019-08-05 19:52:04 +0300

    added footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
 import SuccessStory from './components/Success';
+import FooterPage from './components/Footer';
+
 
 
 function App() {
   return (
     <Container>
       <SuccessStory/>
+      <FooterPage/>
     </Container>
   );
 }
