788958657dc5fe57134c8960c23e759781643be2
taha hussainalash <taha.benzma94@gmail.com>
2019-08-09 18:16:50 +0300

    working footer and logo section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
-=======
 import SuccessStory from './components/Success';
->>>>>>> aa0fb6f07c8ba9f10fa01d3273b464e04e21091d
 import FooterPage from './components/Footer';
 import Logoes from './logos';
 // import ModalGallery from './modal gallery';
@@ -11,20 +8,12 @@ import Logoes from './logos';
 function App() {
   return (
     <div>
-<<<<<<< HEAD
        <Logoes/>
       
       <FooterPage/>
     
       {/* <ModalGallery/> */}
     </div>
-=======
-      <SuccessStory/>
-      <FooterPage/>
-    </div>
-
-
->>>>>>> aa0fb6f07c8ba9f10fa01d3273b464e04e21091d
   );
 }
 
